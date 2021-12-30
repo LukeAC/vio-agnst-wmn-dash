df6685d4f49247afe4d546dc7c512104f6b4b383
Luke Collins <luke.a.collins@outlook.com>
2021-12-30 09:38:24 -0800

    Add data processing script to webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,10 @@
-# FLASK_APP=app.py FLASK_ENV=development FLASK_DEBUG=1 TEMPLATES_AUTO_RELOAD=1 python app.py
+## Run this in terminal
+## FLASK_APP=app.py FLASK_ENV=development FLASK_DEBUG=1 TEMPLATES_AUTO_RELOAD=1 python app.py
 
 
 from flask import Flask, render_template
 from dash import Dash, html, dcc
+from data_processing import survey_year_plot
 
 import plotly.graph_objects as go
 import plotly.express as px
@@ -10,6 +12,9 @@ import plotly.express as px
 app = Dash()   #initialising dash app
 df = px.data.stocks() #reading stock price dataset 
 
+## Should import graph from EDA module here
+## import << viowmn_eda_module >> as viowmn
+## figure_1 = viowmn.Graph1.generate_graph()
 def stock_prices():
     # Function for creating line chart showing Google stock prices over time 
     fig = go.Figure([go.Scatter(x = df['date'], y = df['GOOG'],\
@@ -23,13 +28,22 @@ def stock_prices():
 
  
 app.layout = html.Div(id = 'parent', children = [
-    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
-                                            'marginTop':40,'marginBottom':40}),
-
-        
-        dcc.Graph(id = 'line_plot', figure = stock_prices())    
+    html.H1(
+            id = 'figure_1', 
+            children = 'Figure 1',
+            style = {
+                    'textAlign':'center',
+                    'marginTop':40,
+                    'marginBottom':40
+                }
+        ),
+ 
+    dcc.Graph(
+            id = 'line_plot', 
+            figure = stock_prices()
+        )    
     ]
-                     )
+)
 
 if __name__ == '__main__': 
     app.run_server()
